d403d00d876af1a2982614d88ed86970ca324595
anecthc <anecthc@hotmail.com>
2023-04-15 02:01:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config["DEBUG"] = True
 
 
 # Configure session to use filesystem (instead of signed cookies)
